b6a7000300cd6c52b8d9e680a074e7f0c1df5113
Omofon <abasiomofonudoh@gmail.com>
2026-04-07 11:40:48 +0100

    feat: Create utils with generateId function

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+export const generateId = (length = 12) =>
+  Math.random()
+    .toString(16)
+    .slice(2, 2 + length);
+
+    /**
+     * generateId()     // "62e25f1d52af"  → 12 chars (task IDs
+     * generateId(8)    // "tr0923sj"      → 8 chars  (board IDs
+     * generateId(6)    // "52327576"      → 6 chars  (column IDs)
+     */
